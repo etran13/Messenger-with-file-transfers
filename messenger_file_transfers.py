8e13a8c74033ae02561d4fc4fcd9e46167ba0526
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-06-05 22:00:45 -0500

    Add code for getting command line arguments

---
+++ messenger_file_transfers.py
@@ -0,0 +1,15 @@
+import sys
+import socket
+import threading
+import os
+
+#Access command line arguments
+arguments = sys.argv[1:]
+arg_dict = {}
+for i in range(0, len(arguments), 2):
+    arg_dict[arguments[i]] = arguments[i + 1]
+
+if len(arg_dict) == 1:
+    #Do server setup
+    port_number = arg_dict["-l"]
+    
