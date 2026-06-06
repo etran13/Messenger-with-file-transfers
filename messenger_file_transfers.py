537d889437434eccc3a9c7143aaf5b8e773b40b3
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-06-06 02:37:32 -0500

    Add socket for requests

--- messenger_file_transfers.py
+++ messenger_file_transfers.py
@@ -49,20 +49,21 @@ class FileSender(Thread):
     """
     Sends requested files to the client.
     """
-    def __init__(self, sock):
+    def __init__(self, r_sock, f_sock):
         #print("Sender init")
         Thread.__init__(self)
-        self.sock = sock
+        self.r_sock = r_sock
+        self.f_sock = f_sock
 
     def run(self):
         #print("Send started")
         while True:
-            filename = self.sock.recv(1024).decode()
+            filename = self.r_sock.recv(1024).decode()
 
             #Check if file exists
             file_stat= os.stat(filename)
             if file_stat.st_size:
-                self.sock.sendall("1".encode())
+                self.f_sock.sendall("1".encode())
                 self.send_file(filename)
 
     def send_file(self, filename):
@@ -75,13 +76,13 @@ class FileSender(Thread):
                 print("Sending file...")
                 file_bytes= file.read(1024)
                 if file_bytes:
-                    self.sock.send("1".encode())
-                    client_is_ready = self.sock.recv(1024).decode()
+                    self.f_sock.send("1".encode())
+                    client_is_ready = self.f_sock.recv(1024).decode()
                     if client_is_ready == "1":
-                        self.sock.send(file_bytes)
+                        self.f_sock.send(file_bytes)
                 else:
                     print("Done sending")
-                    self.sock.send("0".encode())
+                    self.f_sock.send("0".encode())
                     break
             file.close()
         except:
@@ -130,16 +131,17 @@ class FileReceiver(Thread):
 # =============================================================================
 
 def accept_connection(portNum):
-    """For server setup: Accepts and returns two connection objects:
-    one for messages and one for files"""
+    """For server setup: Accepts and returns three connection objects:
+    one for messages, one for requests and one for files"""
     print("accept_connection")
     port_int = int(portNum)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as initialServer:
         initialServer.bind(("10.56.2.249", port_int))
         initialServer.listen()
         message_conn, addr = initialServer.accept() 
+        request_conn, addr = initialServer.accept() 
         file_conn, addr = initialServer.accept() 
-        return (message_conn, file_conn)
+        return (message_conn, request_conn, file_conn)
 
 def make_connection(portNum):
     "For client setup: Returns a connection object given a port number"
@@ -169,11 +171,12 @@ for i in range(0, len(arguments), 2):
 if len(arg_dict) == 1:
     #Do server setup
     port_number = arg_dict["-l"]
-    m_conn, f_conn = accept_connection(arg_dict["-l"])
+    m_conn, r_conn, f_conn = accept_connection(arg_dict["-l"])
     print("Server: made")
 else:
     #Do client setup
     m_conn = make_connection(arg_dict["-l"])
+    r_conn = make_connection(arg_dict["-l"])
     f_conn = make_connection(arg_dict["-l"])
     print("Client: made")
 
@@ -185,7 +188,7 @@ f_queue = Queue()
 m_recv = MessageReciever(m_conn)
 m_recv.start()
 
-f_recv = FileSender(f_conn)
+f_recv = FileSender(r_conn, f_conn)
 f_recv.start()
 
 m_send = MessageSender(m_conn, m_queue)
