92d274a961fbc6cff3d2472dcbf006c9479a05b5
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-06-05 22:31:13 -0500

    Add basic connection setup

--- messenger_file_transfers.py
+++ messenger_file_transfers.py
@@ -3,6 +3,20 @@ import socket
 import threading
 import os
 
+def accept_connection(portNum):
+    "For server setup: Accepts and returns a connection object"
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as initialServer:
+        initialServer.bind(("10.56.2.249", portNum))
+        initialServer.listen()
+        conn, addr = initialServer.accept() 
+        return conn
+
+def make_connection(portNum):
+    "For client setup: Returns a connection object given a port number"
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn.connect(("10.56.2.249", portNum)) 
+    return conn
+
 #Access command line arguments
 arguments = sys.argv[1:]
 arg_dict = {}
@@ -12,8 +26,14 @@ for i in range(0, len(arguments), 2):
 if len(arg_dict) == 1:
     #Do server setup
     port_number = arg_dict["-l"]
+
+    #Make and accept connection for messages m and files f
+    m_conn = accept_connection(arg_dict["-l"])
+    f_conn = accept_connection(arg_dict["-l"])
+    print("Server: made")
 else:
-    #Do client setup
-    pass
+    m_conn = make_connection(arg_dict["-l"])
+    f_conn = make_connection(arg_dict["-l"])
+    print("Client: made")
 
 print(arg_dict)
